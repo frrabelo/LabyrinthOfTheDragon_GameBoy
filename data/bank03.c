3d8cadaf67d59de83f47d527b6981cc40786646f
Ryan Richards <sandor.richards@gmail.com>
2024-12-12 17:48:55 -0500

    Moved default battle palettes to the bank 3 data source file.

--- data/bank03.c
+++ data/bank03.c
@@ -5,8 +5,10 @@
 
 #pragma bank 3
 
-#include <stdint.h>
+#include <gb/gb.h>
+#include <gb/cgb.h>
 #include <gbdk/incbin.h>
+#include <stdint.h>
 
 const uint8_t attack_roll_target[65] = {
   50, 54, 58, 63, 67, 72, 76, 81,
@@ -28,6 +30,92 @@ const uint8_t damage_roll_modifier[16] = {
 const uint8_t xp_mod[16] = {
   0, 2, 5, 9, 12, 14, 15, 16,
   16, 17, 18, 20, 23, 27, 30, 32,
+};
+
+const palette_color_t battle_bg_palettes[] = {
+  // Palette 0 - Background / Textbox
+  RGB_WHITE,
+  RGB8(111, 127, 243),
+  RGB8(60, 80, 60),
+  RGB8(28, 28, 0),
+  // Palette 1 - Monster 1
+  RGB_WHITE,
+  RGB8(209, 206, 107),
+  RGB8(126, 73, 73),
+  RGB8(0, 40, 51),
+  // Palette 2 - Monster 2
+  RGB_WHITE,
+  RGB8(209, 206, 107),
+  RGB8(126, 73, 73),
+  RGB8(0, 40, 51),
+  // Palette 3 - Monster 3
+  RGB_WHITE,
+  RGB8(209, 206, 107),
+  RGB8(126, 73, 73),
+  RGB8(0, 40, 51),
+  // Palette 4 - HP Normal
+  RGB_WHITE,
+  RGB8(150, 200, 150),
+  RGB8(80, 120, 80),
+  RGB8(0, 32, 0),
+  // Palette 5 - HP Critical
+  RGB_WHITE,
+  RGB8(200, 150, 150),
+  RGB8(120, 80, 80),
+  RGB8(32, 0, 0),
+  // Palette 6 - Buff
+  RGB8(40, 150, 40),
+  RGB_WHITE,
+  RGB8(120, 120, 120),
+  RGB_BLACK,
+  // Palette 7 - Debuff
+  RGB8(150, 40, 40),
+  RGB_WHITE,
+  RGB8(120, 120, 120),
+  RGB_BLACK,
+};
+
+const palette_color_t battle_sprite_palettes[] = {
+  // Palette 0 - UNUSED
+  RGB_WHITE,
+  RGB_LIGHTGRAY,
+  RGB_DARKGRAY,
+  RGB_BLACK,
+  // Palette 1 - UNUSED
+  RGB_WHITE,
+  RGB_LIGHTGRAY,
+  RGB_DARKGRAY,
+  RGB_BLACK,
+  // Palette 2 - UNUSED
+  RGB_WHITE,
+  RGB_LIGHTGRAY,
+  RGB_DARKGRAY,
+  RGB_BLACK,
+  // Palette 3 - UNUSED
+  RGB_WHITE,
+  RGB_LIGHTGRAY,
+  RGB_DARKGRAY,
+  RGB_BLACK,
+  // Palette 4 - UNUSED
+  RGB_WHITE,
+  RGB_LIGHTGRAY,
+  RGB_DARKGRAY,
+  RGB_BLACK,
+  // Palette 5 - UNUSED
+  RGB_WHITE,
+  RGB_LIGHTGRAY,
+  RGB_DARKGRAY,
+  RGB_BLACK,
+  // Palette 6 - UNUSED
+  RGB_WHITE,
+  RGB_LIGHTGRAY,
+  RGB_DARKGRAY,
+  RGB_BLACK,
+  // Palette 7 - CURSOR
+  RGB_WHITE,
+  RGB_BLACK,
+  RGB_DARKGRAY,
+  RGB_WHITE,
 };
 
 INCBIN(tilemap_battle_menus, "res/tilemaps/battle_menus.tilemap")

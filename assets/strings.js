750aec02313ab1bb7d5c23159675870312486f33
Ryan Richards <sandor.richards@gmail.com>
2024-12-14 18:44:27 -0500

    Added some more battle related strings.

--- assets/strings.js
+++ assets/strings.js
@@ -38,7 +38,7 @@ addNamespace('summon_name', 0, {
 })
 
 /**
- * Ability names.
+ * Ability names, attack strings, etc.
  */
 addNamespace('ability', 0, {
   'vine_whip': 'Vine Whip',
@@ -51,17 +51,23 @@ addNamespace('ability', 0, {
  * Battle related strings.
  */
 addNamespace('battle', 3, {
+  'dirty_fang_attack': 'You rush forward, fangs bared!',
+  'dirty_fang_hit_poison': 'Your bite deals %damage damage and poison them!',
+  'dirty_fang_hit_no_poison': 'Your filthy kobold bite deals %damage damage!',
+  'kobold_base_attack': 'You attack %monster %c with your claws...',
+  'kobold_axe': 'Kobold %c attacks with a stone axe...',
   'monster_attack': ':fight: %monster %c attacks...',
   'monster_magic_attack': ':magic: %monster %c casts a spell...',
   'monster_miss': 'But they miss!',
   'monster_miss_magic': 'But the spell goes astray!',
-  'monster_hit': 'They hit for %damage %aspect damage!',
-  'kobold_axe': 'Kobold %c attacks with a stone axe...',
+  'monster_hit': 'They hit for %damage damage!',
+  'monster_hit_aspect': 'They hit for %damage %aspect damage!',
   'player_miss': 'But you miss!',
   'player_magic_miss': 'But it has no effect!',
-  'dirty_fang_attack': 'You rush forward, fangs bared!',
-  'dirty_fang_hit_poison': 'Your bite deals %damage damage and poison them!',
-  'dirty_fang_hit_no_poison': 'Your filthy kobold bite deals %damage damage!',
+  'player_hit': 'You hit!',
+  'player_hit_immune': "They're completely immune!",
+  'player_hit_resist': "They resist your attack.",
+  'player_hit_vuln': "It deals massive damage!",
 })
 
 // Export the namespaces

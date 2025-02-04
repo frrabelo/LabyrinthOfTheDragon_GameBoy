326d902558d0a885b02ffbeaf881c5717e0c06f3
Ryan Richards <sandor.richards@gmail.com>
2025-02-03 23:33:53 -0500

    Moved debug16 pointer to an alternate location.

--- src/core.c
+++ src/core.c
@@ -2,7 +2,7 @@
 #include "core.h"
 
 uint8_t *debug = (void *)0xB000;
-uint16_t *debug16 = (void *)0xB000;
+uint16_t *debug16 = (void *)0xB800;
 
 const uint8_t map_tile_lookup[64] = {
   0x00, 0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0x0E,

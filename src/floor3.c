dabbf0fc0794ad3a7610165d33bcaa7639fa3fbd
Ryan Richards <sandor.richards@gmail.com>
2025-03-05 20:31:30 -0500

    Did the floor 3 layout.

--- src/floor3.c
+++ src/floor3.c
@@ -7,8 +7,8 @@
 //------------------------------------------------------------------------------
 
 #define ID 99
-#define DEFAULT_X 5
-#define DEFAULT_Y 30
+#define DEFAULT_X 26
+#define DEFAULT_Y 17
 
 //------------------------------------------------------------------------------
 // Maps
@@ -17,7 +17,6 @@
 static const Map maps[] = {
   // id, bank, data, width, height
   { MAP_A, BANK_16, floor_three_data, 32, 32 },
-
   { END },
 };
 
@@ -38,25 +37,24 @@ static const Chest chests[] = {
     NULL,       // Scripting "on open" callback (optional)
   }
   */
-  {
-    CHEST_1,
-    MAP_A, 15, 12, true, true,
-    str_chest_item_2pot_1eth,
-    chest_item_2pot_1eth,
-  },
-  {
-    CHEST_2,
-    MAP_A, 18, 13, false, false,
-    str_chest_item_1pots,
-    chest_item_1pot,
-  },
-  {
-    CHEST_3,
-    MAP_A, 3, 12, false, false,
-    NULL, NULL,
-    chest_add_magic_key,
-  },
-
+  // {
+  //   CHEST_1,
+  //   MAP_A, 15, 12, true, true,
+  //   str_chest_item_2pot_1eth,
+  //   chest_item_2pot_1eth,
+  // },
+  // {
+  //   CHEST_2,
+  //   MAP_A, 18, 13, false, false,
+  //   str_chest_item_1pots,
+  //   chest_item_1pot,
+  // },
+  // {
+  //   CHEST_3,
+  //   MAP_A, 3, 12, false, false,
+  //   NULL, NULL,
+  //   chest_add_magic_key,
+  // },
   { END },
 };
 
@@ -76,28 +74,23 @@ static const Exit exits[] = {
     EXIT_STAIRS   // Type of exit (not sure if we'll use this yet)
   },
   */
-  {MAP_A, 6, 26, MAP_A, 3, 17, DOWN, EXIT_STAIRS},
-  {MAP_A, 3, 17, MAP_A, 6, 26, DOWN, EXIT_STAIRS},
 
-  {MAP_A, 10, 17, MAP_A, 10, 13, UP, EXIT_STAIRS},
-  {MAP_A, 10, 13, MAP_A, 10, 17, DOWN, EXIT_STAIRS},
+  { MAP_A, 26, 14, MAP_A, 4, 17, UP, EXIT_STAIRS },
+  { MAP_A, 4, 17, MAP_A, 26, 14, DOWN, EXIT_STAIRS },
 
-  {MAP_A, 5, 6, MAP_A, 4, 11, DOWN, EXIT_STAIRS},
-  {MAP_A, 4, 11, MAP_A, 5, 6, DOWN, EXIT_STAIRS},
+  { MAP_A, 24, 14, MAP_A, 19, 29, DOWN, EXIT_STAIRS },
+  { MAP_A, 19, 29, MAP_A, 24, 14, DOWN, EXIT_STAIRS },
 
-  {MAP_A, 16, 6, MAP_A, 21, 6, DOWN, EXIT_STAIRS},
-  {MAP_A, 21, 6, MAP_A, 16, 6, DOWN, EXIT_STAIRS},
+  { MAP_A, 28, 14, MAP_A, 12, 6, DOWN, EXIT_STAIRS },
+  { MAP_A, 12, 6, MAP_A, 28, 14, DOWN, EXIT_STAIRS },
 
-  {MAP_A, 27, 9, MAP_A, 14, 11, DOWN, EXIT_STAIRS},
-  {MAP_A, 14, 11, MAP_A, 27, 9, DOWN, EXIT_STAIRS},
+  { MAP_A, 14, 29, MAP_A, 27, 28, UP, EXIT_STAIRS },
+  { MAP_A, 27, 28, MAP_A, 14, 29, DOWN, EXIT_STAIRS },
 
-  {MAP_A, 28, 9, MAP_A, 15, 18, DOWN, EXIT_STAIRS},
-  {MAP_A, 15, 18, MAP_A, 28, 9, DOWN, EXIT_STAIRS},
+  { MAP_A, 17, 6, MAP_A, 3, 5, UP, EXIT_STAIRS },
+  { MAP_A, 3, 5, MAP_A, 17, 6, DOWN, EXIT_STAIRS },
 
-  {MAP_A, 29, 9, MAP_A, 29, 23, DOWN, EXIT_STAIRS},
-  {MAP_A, 29, 23, MAP_A, 29, 9, DOWN, EXIT_STAIRS},
-
-  {MAP_A, 23, 21, MAP_A, 5, 29, UP, EXIT_STAIRS, &bank_floor3},
+  { MAP_A, 4, 12, MAP_A, DEFAULT_X, DEFAULT_Y, UP, EXIT_STAIRS, &bank_floor3},
 
   {END},
 };
@@ -115,12 +108,11 @@ static const Sign signs[] = {
     "Hi there!" // The message to display
   }
   */
-  // TODO: make this sign say a thing or... remove the sign
-  { MAP_A, 13, 6, UP, str_floor_common_tbd },
-  { MAP_A, 24, 9, UP, str_floor_three_lever_puzzle },
-  { MAP_A, 23, 13, UP, str_floor_three_lever_one },
-  { MAP_A, 27, 13, UP, str_floor_three_lever_two },
-  { MAP_A, 16, 28, UP, str_floor_common_steve_jobs },
+  // { MAP_A, 13, 6, UP, str_floor_common_tbd },
+  // { MAP_A, 24, 9, UP, str_floor_three_lever_puzzle },
+  // { MAP_A, 23, 13, UP, str_floor_three_lever_one },
+  // { MAP_A, 27, 13, UP, str_floor_three_lever_two },
+  // { MAP_A, 16, 28, UP, str_floor_common_steve_jobs },
 
   { END },
 };
@@ -129,51 +121,7 @@ static const Sign signs[] = {
 // Levers
 //------------------------------------------------------------------------------
 
-static void lever_check(void) {
-  close_door(DOOR_4);
-  close_door(DOOR_5);
-  close_door(DOOR_6);
-
-  if(is_lever_on(LEVER_8) && is_lever_on(LEVER_7) && is_lever_on(LEVER_6))
-    open_door(DOOR_4);
-
-  if(is_lever_on(LEVER_8) && !is_lever_on(LEVER_7) && is_lever_on(LEVER_6))
-    open_door(DOOR_5);
-
-  if(is_lever_on(LEVER_8) && !is_lever_on(LEVER_7) && !is_lever_on(LEVER_6) && is_lever_on(LEVER_5))
-    open_door(DOOR_6);
-}
-
 static void on_lever_pull(const Lever *lever) {
-  switch (lever->id)
-  {
-  case LEVER_1:
-    open_door(DOOR_1);
-    break;
-  case LEVER_2:
-    toggle_door(DOOR_8);
-    break;
-  case LEVER_3:
-    open_door(DOOR_2);
-    break;
-  case LEVER_4:
-    open_door(DOOR_3);
-    break;
-  case LEVER_5:
-    lever_check();
-    break;
-  case LEVER_6:
-    lever_check();
-    break;
-  case LEVER_7:
-    lever_check();
-    break;
-  case LEVER_8:
-    lever_check();
-    break;
-  default:
-    break;
-  }
 }
 
 static const Lever levers[] = {
@@ -187,16 +135,7 @@ static const Lever levers[] = {
     NULL,     // Scripting callback for the lever
   }
   */
-  { LEVER_1, MAP_A,  4, 28, true, false, on_lever_pull },
-  { LEVER_2, MAP_A, 12, 28, true, false, on_lever_pull },
-  { LEVER_3, MAP_A,  9,  6, true, false, on_lever_pull },
-  { LEVER_4, MAP_A, 11,  6, true, false, on_lever_pull },
-
-  { LEVER_5, MAP_A, 22, 14, false, false, on_lever_pull },
-  { LEVER_6, MAP_A, 24, 14, false, false, on_lever_pull },
-  { LEVER_7, MAP_A, 26, 14, false, false, on_lever_pull },
-  { LEVER_8, MAP_A, 28, 14, false, false, on_lever_pull },
-
+  // { LEVER_1, MAP_A,  4, 28, true, false, on_lever_pull },
   { END },
 };
 
@@ -215,18 +154,7 @@ static const Door doors[] = {
     false,            // Does the door start opened?
   }
   */
-  { DOOR_1, MAP_A,  6, 26, DOOR_NORMAL, false },
-  { DOOR_2, MAP_A,  5,  6, DOOR_NORMAL, false },
-  { DOOR_3, MAP_A, 16,  6, DOOR_NORMAL, false },
-
-  { DOOR_4, MAP_A, 27,  9, DOOR_NORMAL, false },
-  { DOOR_5, MAP_A, 28,  9, DOOR_NORMAL, false },
-  { DOOR_6, MAP_A, 29,  9, DOOR_NORMAL, false },
-
-  { DOOR_7, MAP_A, 23, 21, DOOR_NEXT_LEVEL, false },
-
-  { DOOR_8, MAP_A, 10, 17, DOOR_NORMAL, false },
-
+  // { DOOR_1, MAP_A,  6, 26, DOOR_NORMAL, false },
   { END }
 };
 
@@ -247,12 +175,7 @@ static const Sconce sconces[] = {
     FLAME_BLUE  // Flame color for the sconce if it starts lit.
   }
   */
-  { SCONCE_STATIC, MAP_A, 12, 26, true, FLAME_RED },
-  { SCONCE_STATIC, MAP_A, 14, 18, true, FLAME_RED },
-  { SCONCE_STATIC, MAP_A, 16, 18, true, FLAME_RED },
-  { SCONCE_STATIC, MAP_A, 10,  5, true, FLAME_RED },
-  { SCONCE_STATIC, MAP_A, 25, 13, true, FLAME_RED },
-
+  // { SCONCE_STATIC, MAP_A, 12, 26, true, FLAME_RED },
   { END }
 };
 
@@ -261,42 +184,37 @@ static const Sconce sconces[] = {
 //------------------------------------------------------------------------------
 
 static void on_boss_victory(void) NONBANKED {
-  open_door(DOOR_7);
-  set_npc_invisible(NPC_2);
 }
 
 static void on_elite_victory(void) NONBANKED {
-  grant_ability(ABILITY_1);
-  set_npc_invisible(NPC_1);
-  map_textbox(str_floor_common_new_ability);
 }
 
 static bool on_boss_encouter(void) {
-  Monster *monster = encounter.monsters;
-  reset_encounter(MONSTER_LAYOUT_1);
-  kobold_generator(monster, player.level, B_TIER);
-  monster->id = 'A';
-  set_on_victory(on_boss_victory);
-  start_battle();
+  // Monster *monster = encounter.monsters;
+  // reset_encounter(MONSTER_LAYOUT_1);
+  // kobold_generator(monster, player.level, B_TIER);
+  // monster->id = 'A';
+  // set_on_victory(on_boss_victory);
+  // start_battle();
   return true;
 }
 
 static bool on_elite_encouter(void) {
-  Monster *monster = encounter.monsters;
-  reset_encounter(MONSTER_LAYOUT_1);
-  kobold_generator(monster, player.level, B_TIER);
-  monster->id = 'A';
-  set_on_victory(on_elite_victory);
-  start_battle();
+  // Monster *monster = encounter.monsters;
+  // reset_encounter(MONSTER_LAYOUT_1);
+  // kobold_generator(monster, player.level, B_TIER);
+  // monster->id = 'A';
+  // set_on_victory(on_elite_victory);
+  // start_battle();
   return true;
 }
 
 static bool on_npc_action(const NPC *npc) {
-  if (npc->id == NPC_2){
-    map_textbox_with_action(str_floor_common_fight_me, on_boss_encouter);
-  } else {
-    map_textbox_with_action(str_floor_common_love, on_elite_encouter);
-  }
+  // if (npc->id == NPC_2){
+  //   map_textbox_with_action(str_floor_common_fight_me, on_boss_encouter);
+  // } else {
+  //   map_textbox_with_action(str_floor_common_love, on_elite_encouter);
+  // }
   return true;
 }
 
@@ -311,9 +229,8 @@ static const NPC npcs[] = {
     action_callback,  // Action callback to execute when the player interacts
   }
   */
-  { NPC_1, MAP_A, 15, 20, MONSTER_KOBOLD, A_TIER, on_npc_action },
-  { NPC_2, MAP_A, 23, 22, MONSTER_KOBOLD, S_TIER, on_npc_action },
-
+  // { NPC_1, MAP_A, 15, 20, MONSTER_KOBOLD, A_TIER, on_npc_action },
+  // { NPC_2, MAP_A, 23, 22, MONSTER_KOBOLD, S_TIER, on_npc_action },
   { END }
 };
 
@@ -344,22 +261,19 @@ static const EncounterTable random_encounters[] = {
 
 
 static bool on_init(void) {
-  // TODO: 600 is to high. debugging in progress. revert to lower value.
-  config_random_encounter(600, 1, 1, true);
+  // config_random_encounter(600, 1, 1, true);
   return false;
 }
 
 static bool on_special(void) {
-  if (player_at(20, 15) &&
-      (is_lever_on(LEVER_8) ||
-       is_lever_on(LEVER_7) ||
-       is_lever_on(LEVER_6) ||
-       !is_lever_on(LEVER_5))){
-    teleport(MAP_A, 25, 15, LEFT);
-    return true;
-  }
-
-
+  // if (player_at(20, 15) &&
+  //     (is_lever_on(LEVER_8) ||
+  //      is_lever_on(LEVER_7) ||
+  //      is_lever_on(LEVER_6) ||
+  //      !is_lever_on(LEVER_5))){
+  //   teleport(MAP_A, 25, 15, LEFT);
+  //   return true;
+  // }
   return false;
 }
 
